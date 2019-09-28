14fac932e3d8aa23e3fd574d2ed0f1eb11a277d9
Loide Mara Verdes <mara.verdes@gmail.com>
2019-09-28 03:15:06 -0400

    Refactor to preparer for a tcp listening

--- src/proxy.py
+++ src/proxy.py
@@ -4,50 +4,28 @@ import socket
 import ssl
 import threading
 import logging
-import binascii
 import click
+from multiprocessing import Process
+from udp import Udp
+from config import Config
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+def listen_udp(address, port, dns, ca):
+    """ Listening for DNS UDP requests """
+    try:
+        udp = Udp()
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.bind((address, port))
+        while True:
+            data, address = sock.recvfrom(4096)
+            threading.Thread(
+                    target=udp.handler,
+                    args=(data, address, sock, dns, ca)
+                    ).start()
+    except Exception as e:
+        logging.error(e)
+    finally:
+        sock.close()
 
-def send_message(dns, query, ca_path):
-
-    server = (dns, 853)
-    # tcp socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.settimeout(80)
-
-    ctx = ssl.create_default_context()
-    ctx = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
-    ctx.verify_mode = ssl.CERT_REQUIRED
-    ctx.check_hostname = True
-    ctx.load_verify_locations(ca_path)
-
-    wrapped_socket = ctx.wrap_socket(sock, server_hostname=dns)
-    wrapped_socket.connect(server)
-    logger.info("Server peer certificate: %s", str(wrapped_socket.getpeercert()))
-
-    tcp_msg = "\x00".encode() + chr(len(query)).encode() + query
-    logger.info("Client request: %s", str(tcp_msg))
-    wrapped_socket.send(tcp_msg)
-    data = wrapped_socket.recv(1024)
-    return data
-
-def thread(data, address, socket, dns, ca_path):
-
-    answer = send_message(dns, data, ca_path)
-    if answer:
-        logger.info("Server reply: %s", str(answer))
-        rcode = binascii.hexlify(answer[:6]).decode("utf-8")
-        rcode = rcode[11:]
-        if int(rcode, 16) == 1:
-            logger.error("Error processing the request, RCODE = %s", rcode)
-        else:
-            logger.info("Proxy OK, RCODE = %s", rcode)
-            return_ans = answer[2:]
-            socket.sendto(return_ans, address)
-    else:
-        logger.warn("Empty reply from server.")
 
 @click.command()
 @click.option('-p', '--port',
@@ -65,21 +43,8 @@ def main(port, address, dns, ca):
     """
     DNS to DNS-over-TLS simple deamon.
     """
-
-    try:
-        # udp socket
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.bind((address, port))
-        while True:
-            data, address = sock.recvfrom(4096)
-            threading.Thread(
-                    target=thread,
-                    args=(data, address, sock, dns, ca)
-            ).start()
-    except Exception as e:
-        logger.error(e)
-    finally:
-        sock.close()
+    logging.basicConfig(level=logging.INFO)
+    Process(target=listen_udp(address, port, dns, ca)).start()
 
 if __name__ == "__main__":
     main()
---
+++ src/udp.py
@@ -0,0 +1,52 @@
+import socket
+import ssl
+import logging
+import binascii
+from config import Config
+
+class Udp:
+    def send_message_tls(self, dns, query, ca_path):
+        """ Send request to a secure DNS Server from UDP Socket"""
+        server = (dns, 853) # default port for cloudflare
+
+        # tcp socket
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(80)
+
+        ssl_ctx = ssl.create_default_context()
+        ssl_ctx = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+        ssl_ctx.verify_mode = ssl.CERT_REQUIRED
+        ssl_ctx.check_hostname = True
+        ssl_ctx.load_verify_locations(ca_path)
+
+        wrapped_socket = ssl_ctx.wrap_socket(sock, server_hostname=dns)
+        wrapped_socket.connect(server)
+        logging.info("Server peer certificate: %s", str(wrapped_socket.getpeercert()))
+
+        tcp_msg = "\x00".encode() + chr(len(query)).encode() + query
+        logging.info("Client request: %s", str(tcp_msg))
+        wrapped_socket.send(tcp_msg)
+        data = wrapped_socket.recv(1024)
+
+        return data
+
+    def response(self, answer, address, socket):
+        if answer:
+            logging.info("Server reply: %s", str(answer))
+            rcode = binascii.hexlify(answer[:6]).decode("utf-8")
+            rcode = rcode[11:]
+            if int(rcode, 16) == 1:
+                logging.error("Error processing the request, RCODE = %s", rcode)
+            else:
+                logging.info("Proxy OK, RCODE = %s", rcode)
+                return_ans = answer[2:]
+                socket.sendto(return_ans, address)
+        else:
+            logging.warn("Empty reply from server.")
+
+
+    def handler(self, data, address, socket, dns_addr, ca_path):
+        answer = self.send_message_tls(dns_addr, data, ca_path)
+
+        self.response(answer, address, socket)
+
